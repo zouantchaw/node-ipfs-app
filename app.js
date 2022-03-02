d751b75ac8acb97057fb0446379e878b2422d37e
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-03-02 15:34:05 -0500

    config

---
+++ app.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const ipfsClient = require('ipfs-http-client');
+
+const ipfs = new ipfsClient({ host: 'localhost', port: '5001', protocol: 'http'});
+const app = express()
+
+// Configuration
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded(extended: true));
+app.use(fileUpload());
